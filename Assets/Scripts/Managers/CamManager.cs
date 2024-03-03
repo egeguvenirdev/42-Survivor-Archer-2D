b6d05426097f00f71f2e315a00d2236ff67346be
Ege Güvenir <egeguvenirdev@gmail.com>
2024-03-03 07:12:42 +0300

    cam pos fix

--- Assets/Scripts/Managers/CamManager.cs
+++ Assets/Scripts/Managers/CamManager.cs
@@ -39,6 +39,8 @@ public class CamManager : MonoBehaviour
         ActionManager.Updater -= OnUpdate;
         ActionManager.CamShake -= OnCamShake;
         ActionManager.GetOrtograficScreenToWorldPoint -= OnGetOrtograficCam;
+
+        transform.position = Vector3.zero;
     }
 
     private void OnUpdate(float deltaTime)
